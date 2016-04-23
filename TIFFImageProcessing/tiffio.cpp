c3f5b998bf493e20df58ff7a01573d29b06accee
XIA JING <131250162@smail.nju.edu.cn>
2016-04-23 12:29:55 +0800

    tags interpreted

--- TIFFImageProcessing/tiffio.cpp
+++ TIFFImageProcessing/tiffio.cpp
@@ -15,6 +15,9 @@ unsigned int get_type_size(unsigned short type) {
 	return IFD_TYPE_SIZE[type];
 }
 
+void print_entry(IFD_ENTRY*);
+void get_entry_value(ifstream &ifs, IFD_ENTRY *ent, char byte_order);
+
 TIFF_FILE* tiff_read(const char* path) {
 	ifstream ifs(path);
 	if (!ifs.good()) {
@@ -55,122 +58,111 @@ TIFF_FILE* tiff_read(const char* path) {
 	tfile->ifd_off = readint(ifs, bytord);
 	cout << endl << tfile->ifd_off;
 
-	//read IFDs
-	IFD* ifd_p;
-	//first IFD
+	//read IFDs, at least one IFD
 	ifs.seekg(tfile->ifd_off);
-	ifd_p = new IFD;
-	tfile->ifd_p = ifd_p;
+	TIFF_IMAGE *img_p = new TIFF_IMAGE;
+	tfile->image_p = img_p;
 	while(true) {
+		IFD* ifd_p = new IFD;
+		img_p->ifd_p = ifd_p;
 		ifd_p->entry_c = readshort(ifs, bytord);
 		cout << " entry count:" << ifd_p->entry_c;
 		//read IFD entries
-		ifd_p->ifd_entry_p = new IFD_ENTRY[ifd_p->entry_c];
+		ifd_p->ifd_entries = new IFD_ENTRY[ifd_p->entry_c];
 		for (int i = 0; i < ifd_p->entry_c;i++) {
-			IFD_ENTRY *ent = ifd_p->ifd_entry_p + i;
+			IFD_ENTRY *ent = ifd_p->ifd_entries + i;
 			ent->tag = readshort(ifs, bytord);
 			ent->type = readshort(ifs, bytord);
 			ent->value_c = readint(ifs, bytord);
 			ifs.read((char *)ent->value_off, 4);
-			cout << endl << "tag:" << ent->tag << " type:" << ent->type << " value count:" << ent->value_c;
+			//cout << endl << "tag:" << ent->tag << " type:" << ent->type << " value count:" << ent->value_c;
+
+			//read entry values
+			if (!is_type_valid(ent->type))
+				continue;//ignore unknown type
+			get_entry_value(ifs, ent, bytord);
+			print_entry(ent);
+
+			//intepret IFD entry values
+			unsigned char tsize = get_type_size(ent->type);
+			switch (ent->tag) {
+			case PHOTOMATRIC_INTERPRETATION: {
+				img_p->phtmtrc_intprt = ((unsigned short*)(ent->values))[0];
+				break;
+			}
+			case NEW_SUBFILE_TYPE: {
+				img_p->subfile_type = ((unsigned int*)(ent->values))[0] & 0b111;
+				break;
+			}
+			case IMAGE_WIDTH: {
+				if (tsize == 2)
+					img_p->width = ((unsigned short*)(ent->values))[0];
+				else if (tsize == 4)
+					img_p->width = ((unsigned int*)(ent->values))[0];
+				else img_p->width = 0;
+				break;
+			}
+			case IMAGE_LENGTH: {
+				if (tsize == 2)
+					img_p->length = ((unsigned short*)(ent->values))[0];
+				else if (tsize == 4)
+					img_p->length = ((unsigned int*)(ent->values))[0];
+				else img_p->length = 0;
+				break;
+			}
+			case COMPRESSION: {
+				//TO-DO
+				img_p->compression = ((unsigned short*)(ent->values))[0];
+				break;
+			}
+			case STRIP_OFFSETS: {
+				if (tsize == 2)
+					img_p->strip_off = ((unsigned short*)(ent->values))[0];
+				else if (tsize == 4)
+					img_p->strip_off = ((unsigned int*)(ent->values))[0];
+				else img_p->strip_off = 0;
+				break;
+			}
+			case ROWS_PER_STRIP: {
+				if (tsize == 2)
+					img_p->rows_per_strip = ((unsigned short*)(ent->values))[0];
+				else if (tsize == 4)
+					img_p->rows_per_strip = ((unsigned int*)(ent->values))[0];
+				break;
+			}
+			case STRIP_BYTES_COUNT: {
+				if (tsize == 2)
+					img_p->strip_bytes_c = ((unsigned short*)(ent->values))[0];
+				else if (tsize == 4)
+					img_p->strip_bytes_c = ((unsigned int*)(ent->values))[0];
+				break;
+			}
+			case SAMPLES_PER_PIXEL: {
+				img_p->sample_per_pix = ((unsigned short*)(ent->values))[0];
+				break;
+			}
+			case BITS_PER_SAMPLE: {
+				img_p->bits_per_sample = ((unsigned short*)(ent->values))[0];
+				break;
+			}
+			case PLANAR_CONFIGURATION: {
+				img_p->plannar_conf= ((unsigned short*)(ent->values))[0];
+				break;
+			}
+			}
 		}
+
+		//read next IFD if necessary
 		ifd_p->nx_ifd_off = readint(ifs, bytord);
 		if (ifd_p->nx_ifd_off != 0) {
 			ifs.seekg(ifd_p->nx_ifd_off);
-			ifd_p->nx_ifd_p = new IFD;
-			ifd_p = ifd_p->nx_ifd_p;
+			img_p->nx_image = new TIFF_IMAGE;
+			img_p = img_p->nx_image;
 		}
-		else break;
-	}
-
-	//read IFD entry values
-	ifd_p = tfile->ifd_p;
-	while (true) {
-		for (unsigned int i = 0; i < ifd_p->entry_c;i++) {
-			IFD_ENTRY *ent = ifd_p->ifd_entry_p + i;
-			if (!is_type_valid(ent->type)) 
-				continue;//ignore unknown type
-			unsigned char tsize = get_type_size(ent->type);
-			unsigned int vsize = tsize*ent->value_c;
-			unsigned char *values = new unsigned char[vsize];
-			ent->values = values;
-			if (vsize <= 4) {//TO-DO
-				switch (tsize) {
-				case 1: {
-					for (unsigned char j = 0; j < vsize;j++)
-						ent->values[j] = ent->value_off[j];
-					break;
-				}
-				case 2: {
-					*((unsigned short*)values) = readshort(ent->value_off, bytord);
-					if(ent->value_c == 2)
-						*((unsigned short*)(values + 2)) = readshort(ent->value_off+2, bytord);
-					break;
-				}
-				case 4: {
-					*((unsigned int*)values) = readint(ent->value_off, bytord);
-					break;
-				}
-				}
-			}
-			else {
-				ifs.seekg(readint(ent->value_off, bytord));
-				switch (tsize) {
-				case 1: {
-					for (unsigned int j = 0;j < vsize;j += 1) {
-						values[j] = ifs.get();
-					}
-					break;
-				}
-				case 2: {
-					for (unsigned int j = 0;j < vsize;j += 2) {
-						unsigned short sh = readshort(ifs, bytord);
-						*((unsigned short*)(values + j)) = sh;					
-					}
-					break;
-				}
-				case 4: {
-					for (unsigned int j = 0;j < vsize;j += 4) {
-						unsigned int in = readint(ifs, bytord);
-						*((unsigned int*)(values + j)) = in;
-					}
-					break;
-				}
-				case 8: {
-					for (unsigned int j = 0;j < vsize;j += 8) {
-						unsigned long long lo = readlong(ifs, bytord);
-						*((unsigned long long*)(values + j)) = lo;
-					}
-					break;
-				}
-				}
-			}		
-
-			unsigned short test[] = { 1,256 };
-			cout << endl << "tag:" << ent->tag << " type:" << ent->type << " value count:" << ent->value_c;
-			for (unsigned int j = 0; j < ent->value_c;j++) {
-				cout << " value:";
-				switch(tsize) {
-				case 1:
-					cout << (unsigned int)(ent->values[j]);
-					break;
-				case 2:
-					cout << *(((unsigned short*)(ent->values))+j);
-					break;
-				case 4:
-					cout << (((unsigned int*)(ent->values))[j]);
-					break;
-				case 8:
-					cout << (((unsigned long long*)(ent->values))[j]);
-					break;
-				}
-			}
+		else {
+			img_p->nx_image = 0;
+			break;
 		}
-
-		if (ifd_p->nx_ifd_p != 0) {
-			ifd_p = ifd_p->nx_ifd_p;
-		}
-		else break;
 	}
 
 	ifs.close();
@@ -182,14 +174,98 @@ void tiff_delete(TIFF_FILE *tiff) {
 	if (tiff == nullptr)
 		return;
 
-	IFD *ifdp = tiff->ifd_p;	
+	TIFF_IMAGE *img_p = tiff->image_p;
 	do {
-		IFD *nxp = ifdp->nx_ifd_p;
-		delete[] ifdp->ifd_entry_p;
+		IFD* ifdp = img_p->ifd_p;
+		for (int i = 0;i < ifdp->entry_c;i++)
+			delete[] ifdp->ifd_entries[i].values;
+		delete[] ifdp->ifd_entries;
 		delete ifdp;
-		ifdp = nxp;
-	} while (ifdp != 0);
+
+		TIFF_IMAGE *nxp = img_p->nx_image;
+		delete img_p;
+		img_p = nxp;
+	} while (img_p != 0);
 	
 	delete tiff;
+}
+
+void get_entry_value(ifstream &ifs, IFD_ENTRY *ent, char byte_order) {
+	unsigned char tsize = get_type_size(ent->type);
+	unsigned int vsize = tsize*ent->value_c;
+	unsigned char *values = new unsigned char[vsize];
+	ent->values = values;
+	if (vsize <= 4) {
+		switch (tsize) {
+		case 1: {
+			for (unsigned char j = 0; j < vsize;j++)
+				ent->values[j] = ent->value_off[j];
+			break;
+		}
+		case 2: {
+			*((unsigned short*)values) = readshort(ent->value_off, byte_order);
+			if (ent->value_c == 2)
+				*((unsigned short*)(values + 2)) = readshort(ent->value_off + 2, byte_order);
+			break;
+		}
+		case 4: {
+			*((unsigned int*)values) = readint(ent->value_off, byte_order);
+			break;
+		}
+		}
+	}
+	else {
+		ifs.seekg(readint(ent->value_off, byte_order));
+		switch (tsize) {
+		case 1: {
+			for (unsigned int j = 0;j < vsize;j += 1) {
+				values[j] = ifs.get();
+			}
+			break;
+		}
+		case 2: {
+			for (unsigned int j = 0;j < vsize;j += 2) {
+				unsigned short sh = readshort(ifs, byte_order);
+				*((unsigned short*)(values + j)) = sh;
+			}
+			break;
+		}
+		case 4: {
+			for (unsigned int j = 0;j < vsize;j += 4) {
+				unsigned int in = readint(ifs, byte_order);
+				*((unsigned int*)(values + j)) = in;
+			}
+			break;
+		}
+		case 8: {
+			for (unsigned int j = 0;j < vsize;j += 8) {
+				unsigned long long lo = readlong(ifs, byte_order);
+				*((unsigned long long*)(values + j)) = lo;
+			}
+			break;
+		}
+		}
+	}
+}
+
+void print_entry(IFD_ENTRY* ent) {
+	cout << endl << "tag:" << ent->tag << " type:" << ent->type << " value count:" << ent->value_c;
+	for (unsigned int j = 0; j < ent->value_c;j++) {
+		cout << " value:";
+		switch (get_type_size(ent->type)) {
+		case 1:
+			cout << (unsigned int)(ent->values[j]);
+			break;
+		case 2:
+			cout << *(((unsigned short*)(ent->values)) + j);
+			break;
+		case 4:
+			cout << (((unsigned int*)(ent->values))[j]);
+			break;
+		case 8:
+			cout << (((unsigned long long*)(ent->values))[j]);
+			break;
+		}
+	}
 }
 
--- TIFFImageProcessing/tiffio.h
+++ TIFFImageProcessing/tiffio.h
@@ -3,6 +3,7 @@
 #ifndef TIFFIO_H
 #define TIFFIO_H
 
+/*
 //field types
 #define IFD_TYPE_BYTE		1
 #define IFD_TYPE_ASCII		2
@@ -18,7 +19,46 @@
 #define IFD_TYPE_DOUBLE		12
 //boundary
 #define IFD_TYPE_BOUND		13
+*/
 
+enum IFD_TYPE{
+	IFD_TYPE_BYTE		= 1,
+	IFD_TYPE_ASCII		= 2,
+	IFD_TYPE_SHORT		= 3,
+	IFD_TYPE_LONG		= 4,
+	IFD_TYPE_RATIONAL	= 5,
+	IFD_TYPE_SBYTE		= 6,
+	IFD_TYPE_UNDEFINED	= 7,
+	IFD_TYPE_SSHORT		= 8,
+	IFD_TYPE_SLONG		= 9,
+	IFD_TYPE_SRATIONAL	= 10,
+	IFD_TYPE_FLOAT		= 11,
+	IFD_TYPE_DOUBLE		= 12,
+	//boundary
+	IFD_TYPE_BOUND		= 13
+};
+
+enum IFD_ENTRY_TAG {
+	NEW_SUBFILE_TYPE	= 254,
+	IMAGE_WIDTH			= 256,
+	IMAGE_LENGTH		= 257,
+	BITS_PER_SAMPLE		= 258,
+	COMPRESSION			= 259,
+	PHOTOMATRIC_INTERPRETATION = 262,
+	STRIP_OFFSETS		= 273,
+	SAMPLES_PER_PIXEL	= 277,
+	ROWS_PER_STRIP		= 278,
+	STRIP_BYTES_COUNT	= 279,
+	PLANAR_CONFIGURATION = 284
+};
+
+enum PHOTOMATRIC_INTERPRETATION_TYPE {
+	WHITE_IS_ZERO	= 0,
+	BLACK_IS_ZERO	= 1,
+	RGB				= 2,
+	PALETTE_COLOR	= 3,
+	TRASPANRANCY_MASK = 4
+};
 
 struct IFD_ENTRY {
 	unsigned short	tag;
@@ -30,20 +70,42 @@ struct IFD_ENTRY {
 
 struct IFD {
 	unsigned short	entry_c;
-	IFD_ENTRY*		ifd_entry_p	= 0;
+	IFD_ENTRY*		ifd_entries	= 0;
 	unsigned int	nx_ifd_off	= 0;
-	IFD*			nx_ifd_p	= 0;
 };
 
 #define LITTLE_ENDIAN_S	"II"
 #define BIG_ENDIAN_S	"MM"
 
+struct TIFF_IMAGE {
+	IFD* ifd_p = 0;
+
+	//basics
+	unsigned char phtmtrc_intprt;
+	unsigned int width;
+	unsigned int length;
+	unsigned char compression	= 1;
+	//strip
+	unsigned int strip_off;
+	unsigned int rows_per_strip = 0xffff - 1;
+	unsigned int strip_bytes_c = 0;
+	//sample
+	unsigned char sample_per_pix	= 0;
+	unsigned char bits_per_sample	= 0;
+	//mischevious
+	unsigned char subfile_type = 0;
+	unsigned char plannar_conf	= 1;
+
+	TIFF_IMAGE* nx_image;
+};
+
 struct TIFF_FILE {
 	char			byte_order[3];
 	unsigned short	tiff_sign;
 	unsigned int	ifd_off		= 0;
-	IFD*			ifd_p		= 0;
+	TIFF_IMAGE*		image_p		= 0;
 };
+
 
 TIFF_FILE* tiff_read(const char* path);
 void tiff_delete(TIFF_FILE *tiff);
